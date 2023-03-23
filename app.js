285e5751a3567912b46fb2e67969f623056f2674
SophieHsieh96 <yun.97096@gmail.com>
2023-03-23 11:21:30 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 //載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose') //載入mongoose
+const exphbs = require('express-handlebars')
+const app = express()
+
 
 // 僅在非正式環境時，使用 dotenv
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 
-const app = express()
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到 mongoDB
 
 //取得資料庫連線狀態
@@ -21,9 +24,12 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
 //設定首頁路由
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.render('index')
 })
 
 //設定port 3000
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -5,10 +5,15 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 const db = mongoose.connection
+
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
+
+
 db.once('open', () => {
     console.log('mongodb connected!')
     for (let i = 0; i < 10; i++) {
